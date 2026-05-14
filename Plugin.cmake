6b7485ea1e1622888b2eecf954d556f0f826cfeb
Warren Holybee <warren@holybee.net>
2026-05-13 20:41:34 -0700

    Change PLUGIN API NAME

--- Plugin.cmake
+++ Plugin.cmake
@@ -37,7 +37,7 @@ set(PKG_RELEASE 2)
 set(PKG_PRERELEASE "")  # Empty, or a tag like 'beta'
 
 set(DISPLAY_NAME "GPX Export")    # Dialogs, installer artifacts, ...
-set(PLUGIN_API_NAME gpx_export_pi) # As of GetCommonName() in plugin API
+set(PLUGIN_API_NAME "GPX Export") # As of GetCommonName() in plugin API
 set(PKG_SUMMARY "Export GPX to File or FTP")
 set(PKG_DESCRIPTION [=[
 	Export OpenCPN routes and waypoints to GPX files using configurable compatibility presets for marine chartplotters and navigation software.
